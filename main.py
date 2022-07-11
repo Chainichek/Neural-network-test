eb4dc2cb25631997dad3491858fbb681b806be93
Chainichek <daniyar-shaymardanov@yandex.ru>
2022-07-11 16:49:55 +0400

    Пробная версия

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from dataBase import dataBase
 from GUI import *
 from globals import *
 
-import numpy
-import matplotlib.pyplot
+def main():
+    NN = nNetwork_class(input_nodes, hidden_nodes, output_nodes, learning_rate)
+    trainData = dataBase(path)
 
-NN = nNetwork_class(input_nodes, hidden_nodes, output_nodes, learning_rate)
-trainData = dataBase(path)
+    trainData.loadData(NN, epoches)
+    #trainData.testData(NN, 1000)
+    #print(trainData.getEfficiency())
 
-trainData.loadData(NN, epoches)
-#trainData.testData(NN, 1000)
-#print(trainData.getEfficiency())
-
-gui = window(NN)
+    gui = window(NN)
+main()
 
 
 
